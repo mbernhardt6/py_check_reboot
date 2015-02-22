0d597b85787860af26f2efccb74928ef6f21be27
mbernhardt6 <mbernhardt6@gmail.com>
2015-02-21 23:44:44 -0800

    Initial Draft
    Python script to check if a linux server requires a reboot based on
    active kernel version vs latest kernel version installed via rpm.
    Currently intended to run on a CentOS 6.x server.
    
    Really only intended for my own purposes because I'm lazy and don't
    always check my linux boxes for if they need a reboot based on logwatch
    emails. This is a more intrusive way of nagging me.

---
+++ check_reboot.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python2.7
+
+#Homegrown Modules
+import logger
+import mailer
+#Python Modules
+import socket
+import subprocess
+
+#Variables
+hostname = socket.gethostname()
+recipeint = "mark@transcendedlife.local"
+sender = "%s@transcendedlife.local" % hostname
+log = "/var/log/check_reboot.log"
+
+if __name__ == "__main__":
+  logger.logMessage(log, "Checking for required reboot.")
+  latest_kernel = (subprocess.check_output("rpm -q --last kernel | perl -pe 's/^kernel-(\S+).*/$1/' | head -1", shell=True)).strip()
+  active_kernel = (subprocess.check_output("uname -r", shell=True)).strip()
+  logger.logMessage(log, "Active Kernel: %s" % active_kernel)
+  logger.logMessage(log, "Latest Kernel: %s" % latest_kernel)
+
+  if (active_kernel != latest_kernel):
+    logger.logMessage(log, "Required reboot detected. Sending mail.")
+    mailer.sendMail(recipeint, sender, "%s requires reboot." % hostname,
+                    """Newer Kernel version installed than is actively running.
+Reboot %s to resolve.""" % hostname)
+  else:
+    logger.logMessage(log, "No reboot required.")
